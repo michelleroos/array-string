253ae0c2241d9dfda8b12fff3cca04f9608ac033
michelleroos <hello.michelleroos@gmail.com>
2022-01-21 09:52:30 -0500

    intersection

--- intersections.js
+++ intersections.js
@@ -10,38 +10,38 @@ You may assume that each input array does not contain duplicate elements.
 
 const intersection = (a, b) => {
   const intersects = [];
-  if (a.length < b.length) {
-    for (let e of a) {
-      if (b.includes(e)) intersects.push(e);
-    };
-  } else {
-    for (let e of b) {
-      if (a.includes(e)) intersects.push(e);
-    };
+  for (let e of a) {
+    if (b.includes(e)) intersects.push(e);
   };
   return intersects;
 };
 
-// 
+// OPTIMIZED USING SET
+// T: O(m+n) S: O(min(m, n))
 
 const intersection = (a, b) => {
-
-}
+  const intersects = [];
+  const setA = new Set(a);
+  for (let e of b) {
+    if (setA.has(e)) intersects.push(e);
+  };
+  return intersects;
+};
 
 // test_00:
-console.log(intersection([4,2,1,6], [3,6,9,2,10])) 
+console.log(intersection([4, 2, 1, 6], [3, 6, 9, 2, 10]))
 // -> [2,6]
 
 // test_01:
-console.log(intersection([2,4,6], [4,2])) 
+console.log(intersection([2, 4, 6], [4, 2]))
 // -> [2,4]
 
 // test_02:
-console.log(intersection([4,2,1], [1,2,4,6])) 
+console.log(intersection([4, 2, 1], [1, 2, 4, 6]))
 // -> [1,2,4]
 
 // test_03:
-console.log(intersection([0,1,2], [10,11])) 
+console.log(intersection([0, 1, 2], [10, 11]))
 // -> []
 
 // test_04:
@@ -51,5 +51,5 @@ for (let i = 0; i < 50000; i += 1) {
   a.push(i);
   b.push(i);
 }
-console.log(intersection(a, b)) 
+console.log(intersection(a, b))
 // -> [0,1,2,3,..., 49999]
